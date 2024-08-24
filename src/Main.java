140a8fa5b62a1154ebc580df1e923c1f1261121e
GayratAshirov <ashirov.dev@gmail.com>
2024-08-24 15:44:24 +0500

    for added in main method

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println((i+1) + "-xona");
+        }
+
+    }
+}
